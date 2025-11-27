feef477cd0932ab34f7a7fce46857343e42597cb
fallingFor12 <2903533920@qq.com>
2025-11-27 17:47:25 +0800

    微信小程序商品浏览业务代码以及微信登录功能

---
+++ sky-server/src/main/java/com/sky/service/impl/UserServiceImpl.java
@@ -0,0 +1,68 @@
+package com.sky.service.impl;
+
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONObject;
+import com.sky.constant.MessageConstant;
+import com.sky.dto.UserLoginDTO;
+import com.sky.entity.User;
+import com.sky.exception.LoginFailedException;
+import com.sky.mapper.UserMapper;
+import com.sky.properties.WeChatProperties;
+import com.sky.service.UserService;
+import com.sky.utils.HttpClientUtil;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.sky.service.UserService;
+
+import java.time.LocalDateTime;
+import java.util.HashMap;
+import java.util.Map;
+
+@Service
+public class UserServiceImpl implements UserService {
+
+    //微信服务接口地址
+    public static final String WX_LOGIN_URL = "https://api.weixin.qq.com/sns/jscode2session";
+
+    @Autowired
+    private UserMapper userMapper;
+    @Autowired
+    private WeChatProperties weChatProperties;
+    /**
+     * 微信登录
+     * @param userLoginDTO
+     * @return
+     */
+    @Override
+    public User wxLogin(UserLoginDTO userLoginDTO) {
+        //调用微信接口服务获取用户信息OpenID
+        Map<String, String> query = new HashMap<>();
+        query.put("appid", weChatProperties.getAppid());
+        query.put("secret",weChatProperties.getSecret());
+        query.put("js_code",userLoginDTO.getCode());
+        query.put("grant_type","authorization_code");
+        String json = HttpClientUtil.doGet(WX_LOGIN_URL, query);
+
+        JSONObject jsonObject = JSON.parseObject(json);
+        String openid = jsonObject.getString("openid");
+
+        //判断openid是否为空，如果为空则登录失败，抛出异常
+        if (openid == null){
+            throw new LoginFailedException(MessageConstant.LOGIN_FAILED);
+        }
+
+        //判断当前用户是否为新用户
+        User user = userMapper.getByOpenid(openid);
+        //是新用户，自动完成注册
+        if (user == null){
+            user = User.builder()
+                    .openid(openid)
+                    .createTime(LocalDateTime.now())
+                    .build();
+            userMapper.insert(user);
+        }
+        //返回用户信息（对象）
+        return user;
+    }
+}
